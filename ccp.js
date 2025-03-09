ebcc4e800483c344eb283b87db7e91659c064421
nino8386 <Trumtricklohma@gmail.com>
2025-03-09 13:53:22 +0700

    Update ccp.js

--- ccp.js
+++ ccp.js
@@ -1,5 +1,13 @@
 let obj = JSON.parse($response.body);
+
+// Giả lập gói Pro hợp lệ
 obj.isPro = true;
 obj.subscription = "lifetime";
 obj.expiry = "2099-12-31T23:59:59Z";
+
+// Thêm thông tin đăng ký để tránh lỗi xác minh
+obj.planType = "VIP";
+obj.status = "ACTIVE";
+obj.purchaseToken = "fake_purchase_token_12345";
+
 $done({body: JSON.stringify(obj)});
