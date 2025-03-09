0324756874cccf9caa279214d50719d12504704a
nino8386 <Trumtricklohma@gmail.com>
2025-03-09 13:55:27 +0700

    Update deleteheader.js

--- deleteheader.js
+++ deleteheader.js
@@ -1,3 +1,10 @@
 var modifiedHeaders = $request.headers;
+
+// Xóa các header có thể ảnh hưởng đến xác minh Pro
 delete modifiedHeaders["User-Agent"];
+delete modifiedHeaders["Authorization"];
+delete modifiedHeaders["X-CapCut-Auth"];
+delete modifiedHeaders["Referer"];
+delete modifiedHeaders["Cookie"];
+
 $done({headers: modifiedHeaders});
