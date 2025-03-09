d17f43ea2af0460c52d90fe54878128f60b3f837
nino8386 <Trumtricklohma@gmail.com>
2025-03-09 14:29:16 +0700

    Update ccp.js

--- ccp.js
+++ ccp.js
@@ -1,13 +1,6 @@
 let obj = JSON.parse($response.body);
-
-// Giả lập gói Pro hợp lệ
 obj.isPro = true;
 obj.subscription = "lifetime";
 obj.expiry = "2099-12-31T23:59:59Z";
-
-// Thêm thông tin đăng ký để tránh lỗi xác minh
-obj.planType = "VIP";
-obj.status = "ACTIVE";
-obj.purchaseToken = "fake_purchase_token_12345";
-
+console.log("CapCut Pro Patch Applied"); // Kiểm tra log
 $done({body: JSON.stringify(obj)});
