bc9c49bbea46f6c9321cd02610b92ab198527cca
nino8386 <Trumtricklohma@gmail.com>
2025-03-12 21:41:57 +0700

    Update deleteheader.js

--- deleteheader.js
+++ deleteheader.js
@@ -1,9 +1,4 @@
-if ($response && $response.body) {
-    var obj = JSON.parse($response.body);
-    obj.isPro = true;
-    obj.subscription = "lifetime";
-    obj.expiry = "2099-12-31T23:59:59Z";
-    $done({body: JSON.stringify(obj)});
-} else {
-    $done({});
-}
+var modifiedHeaders = $request.headers;
+delete modifiedHeaders["User-Agent"];
+delete modifiedHeaders["Authorization"];
+$done({ headers: modifiedHeaders });
