a2258771134d85e4cb8daa6c2de5bb953a79e8f0
nino8386 <Trumtricklohma@gmail.com>
2025-03-12 21:21:43 +0700

    Update deleteheader.js

--- deleteheader.js
+++ deleteheader.js
@@ -1,19 +1,4 @@
-let obj = JSON.parse($response.body);
-
-// Giả lập gói Pro hợp lệ
-obj.isPro = true;
-obj.subscription = "lifetime";
-obj.expiry = "2099-12-31T23:59:59Z";
-
-// Thêm thông tin đăng ký để tránh lỗi xác minh
-obj.planType = "VIP";
-obj.status = "ACTIVE";
-obj.purchaseToken = "fake_token_12345";
-obj.features = ["premium", "no_ads", "hd_export"];
-obj.valid = true;
-
-// Ghi log kiểm tra
-console.log("CapCut Pro Activated!");
-
-$done({body: JSON.stringify(obj)});
-
+var modifiedHeaders = $request.headers;
+delete modifiedHeaders["User-Agent"];
+delete modifiedHeaders["Authorization"];
+$done({ headers: modifiedHeaders });
