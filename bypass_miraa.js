b1ac45067e431cdf1da089e6a063ea1da2061fcb
nino8386 <Trumtricklohma@gmail.com>
2025-03-12 21:40:26 +0700

    Update bypass_miraa.js

--- bypass_miraa.js
+++ bypass_miraa.js
@@ -1,5 +1,5 @@
 let obj = JSON.parse($response.body);
-obj.subscription = "lifetime";
 obj.isPro = true;
+obj.subscription = "lifetime";
 obj.expiry = "2099-12-31T23:59:59Z";
 $done({ body: JSON.stringify(obj) });
