bfbec995a6e7b8b6df32217359a01f1000b723e9
nino8386 <Trumtricklohma@gmail.com>
2025-03-13 00:57:38 +0700

    Update deleteheader.js

--- deleteheader.js
+++ deleteheader.js
@@ -1,6 +1,5 @@
 var modifiedHeaders = $request.headers;
-if (!$request.url.includes("users/email_account")) {
+if (!$request.url.includes("users/email_account") && !$request.url.includes("users/verify_email")) {
     delete modifiedHeaders["User-Agent"];
-    delete modifiedHeaders["Authorization"];
 }
 $done({ headers: modifiedHeaders });
