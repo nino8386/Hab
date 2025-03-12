d90fa36e2209fcb29bf4e88039ba418f54653c60
nino8386 <Trumtricklohma@gmail.com>
2025-03-12 21:31:50 +0700

    Update log_miraa.js

--- log_miraa.js
+++ log_miraa.js
@@ -1,17 +1,9 @@
-let url = $request.url;
-let method = $request.method;
-let headers = JSON.stringify($request.headers, null, 2);
-let body = $request.body ? JSON.stringify($request.body, null, 2) : "No Body";
-
-let responseBody = $response ? $response.body : "No Response Body";
-let statusCode = $response ? $response.status : "No Status";
-
-console.log(`\n[📌 Miraa API Log]`);
-console.log(`🔹 URL: ${url}`);
-console.log(`🔹 Method: ${method}`);
-console.log(`🔹 Headers: ${headers}`);
-console.log(`🔹 Request Body: ${body}`);
-console.log(`🔹 Response Status: ${statusCode}`);
-console.log(`🔹 Response Body: ${responseBody}`);
-
-$done({body: responseBody});
+if ($response && $response.body) {
+    var obj = JSON.parse($response.body);
+    obj.quota = 999999;
+    obj.remaining = 999999;
+    obj.isPro = true;
+    $done({body: JSON.stringify(obj)});
+} else {
+    $done({});
+}
